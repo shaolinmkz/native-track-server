07a06ac03741f9fb9c86b5b5f20eea55351610c1
shaolinmkz007 <emeka@softcom.ng>
2020-10-30 20:31:02 +0100

    change connection text

--- src/index.js
+++ src/index.js
@@ -7,20 +7,20 @@ dotenv.config();
 const app = express();
 
 mongoose.connect(process.env.CONNECTION_URI, {
-	useNewUrlParser: true,
-	useCreateIndex: true,
+  useNewUrlParser: true,
+  useCreateIndex: true,
 });
 
-mongoose.connection.on('connected', () => {
-    console.log('Connected to DB')
+mongoose.connection.on("connected", () => {
+  console.log("Connected to the mongoDB instance");
 });
 
-mongoose.connection.on('error', () => {
-    console.log('Error connecting to DB')
-})
+mongoose.connection.on("error", (err) => {
+  console.log("Error connecting to mongoDB", err);
+});
 
 app.get("/", (req, res) => {
-	res.send("Hi there");
+  res.send("Hi there");
 });
 
 const PORT = 3000;
